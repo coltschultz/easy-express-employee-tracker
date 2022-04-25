27122e406e2ce241aee5408fa62cdbdc52d97e25
Colt <98503459+coltschultz@users.noreply.github.com>
2022-04-25 00:18:12 -0500

    add routes for roles and departments

--- db/schema.sql
+++ db/schema.sql
@@ -1,12 +1,18 @@
+DROP TABLE IF EXISTS employees;
+DROP TABLE IF EXISTS roles;
+DROP TABLE IF EXISTS departments;
+
+
 CREATE TABLE departments (
     id INTEGER AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(30) NOT NULL,
+    name VARCHAR(30) NOT NULL
 );
 
 CREATE TABLE roles (
     id INTEGER AUTO_INCREMENT PRIMARY KEY,
     job_title VARCHAR(30) NOT NULL,
     salary INTEGER NOT NULL,
+    dept_id INTEGER,
     CONSTRAINT fk_dept FOREIGN KEY (dept_id) REFERENCES departments(id) ON DELETE SET NULL
 );
 
@@ -14,6 +20,6 @@ CREATE TABLE employees (
     id INTEGER AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
-    -- add foreign keys for job titles, departments, salaries, and managers that the employees report to
+    role_id INTEGER,
     CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES roles(id) ON DELETE SET NULL
 );
--- server.js
+++ server.js
@@ -1,24 +1,132 @@
+// Next steps... add route for adding an employee. test post routes.
+
 const express = require('express');
-// const db = require('./db/connection');
-// const apiRoutes = require('./routes/apiRoutes');
+const mysql = require('mysql2');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
-
-const mysql = require('mysql2');
 
 const db = mysql.createConnection({
   host: 'localhost',
   user: 'root',
   password: 'NewPassword',
-  database: 'election'
+  database: 'employees'
 });
 
 // Middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-// (Not Found)
+// GET all departments
+app.get('/api/departments', (req, res) => {
+const sql = `SELECT * FROM departments`;
+
+db.query(sql, (err, rows) => {
+    if (err) {
+    res.status(500).json({ error: err.message });
+    return;
+    }
+    res.json({
+    message: 'success',
+    data: rows
+    });
+});
+});
+
+// GET Department by ID
+app.get('/api/department/:id', (req, res) => {
+    const sql = `SELECT * FROM departments WHERE id = ?`;
+    const params = [req.params.id];
+  
+    db.query(sql, params, (err, row) => {
+      if (err) {
+        res.status(400).json({ error: err.message });
+        return;
+      }
+      res.json({
+        message: 'success',
+        data: row
+      });
+    });
+  });
+
+
+// GET role by id
+app.get('/api/role/:id', (req, res) => {
+    const sql = `SELECT * FROM roles WHERE id = ?`;
+    const params = [req.params.id];
+  
+    db.query(sql, params, (err, row) => {
+      if (err) {
+        res.status(400).json({ error: err.message });
+        return;
+      }
+      res.json({
+        message: 'success',
+        data: row
+      });
+    });
+  });
+
+// Create a department
+app.post('/api/department', ({ body }, res) => {
+    // const errors = inputCheck(
+    //   body,
+    //   'name'
+    // );
+    // if (errors) {
+    //   res.status(400).json({ error: errors });
+    //   return;
+    // }
+  
+    const sql = `INSERT INTO departments (name)
+      VALUES (?)`;
+    const params = [body.name];
+  
+    db.query(sql, params, (err, result) => {
+      if (err) {
+        res.status(400).json({ error: err.message });
+        return;
+      }
+      res.json({
+        message: 'success',
+        data: body
+      });
+    });
+  });
+
+// Create a role
+app.post('/api/role', ({ body }, res) => {
+    // const errors = inputCheck(
+    //   body,
+    //   'job_title',
+    //   'salary',
+    //   'department'
+    // );
+    // if (errors) {
+    //   res.status(400).json({ error: errors });
+    //   return;
+    // }
+  
+    const sql = `INSERT INTO roles (job_title, salary, dept_id)
+      VALUES (?,?,?)`;
+    const params = [body.job_title, body.salary, body.dept_id];
+  
+    db.query(sql, params, (err, result) => {
+      if (err) {
+        res.status(400).json({ error: err.message });
+        return;
+      }
+      res.json({
+        message: 'success',
+        data: body
+      });
+    });
+  });
+  
+
+
+// Catch All
 app.use((req, res) => {
     res.status(404).end();
   });
@@ -31,4 +139,5 @@ db.connect(err => {
       console.log(`Server running on port ${PORT}`);
     });
   });
+
 
