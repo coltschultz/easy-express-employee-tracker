e704cb37bad46fa61a2b389b8f7ff7be115362d7
Colt <98503459+coltschultz@users.noreply.github.com>
2022-04-30 15:25:02 -0500

    remove unneccesary code

--- db/seeds.sql
+++ db/seeds.sql
@@ -21,11 +21,11 @@ VALUES
 INSERT INTO employees
     (first_name, last_name, role_id, manager_id)
 VALUES
-    ('John', 'Doe', 1, 1),
+    ('John', 'Doe', 1, NULL),
     ('Mike', 'Chan', 2, 1),
-    ('Ashley', 'Rodriguez', 3, 1),
-    ('Kevin', 'Tupik', 4, 1),
-    ('Kunal', 'Singh', 5, 1),
-    ('Malia', 'Brown', 6, 1),
-    ('Sarah', 'Lourd', 7, 1),
-    ('Tom', 'Allen', 8, 1);
+    ('Ashley', 'Rodriguez', 3, NULL),
+    ('Kevin', 'Tupik', 4, 3),
+    ('Kunal', 'Singh', 5, NULL),
+    ('Malia', 'Brown', 6, 5),
+    ('Sarah', 'Lourd', 7, NULL),
+    ('Tom', 'Allen', 8, 7);
